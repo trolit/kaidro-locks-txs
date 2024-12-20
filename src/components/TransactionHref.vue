9a47358178e6f3c08352d8a16e52e99e08a6031a
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-12-20 19:38:15 +0100

    Create TransactionHref.vue

---
+++ src/components/TransactionHref.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-link :href="getHref(transactionHash)" target="_blank" type="primary">
+    {{ text }}
+  </el-link>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    transactionHash: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: false,
+      default: () => "txs",
+    },
+  },
+
+  data() {
+    const TX_URL = process.env.FARM_TX_URL;
+
+    return {
+      TRANSACTION_DETAILS_URL: TX_URL,
+    };
+  },
+
+  methods: {
+    getHref(blockHash: string) {
+      return `${this.TRANSACTION_DETAILS_URL}/${blockHash}`;
+    },
+  },
+};
+</script>
