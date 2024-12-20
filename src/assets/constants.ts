afe6a2d6c9a06b7f8e394275b8e6ae4073b1b02c
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-12-20 12:44:13 +0100

    save data

---
+++ src/assets/constants.ts
@@ -0,0 +1,8 @@
+export const MAX_PAGES_TO_FETCH_LIMIT = 15; // extra protection in case of issue when fetching transactions
+
+export const LAST_SYNERGY_BEFORE_TGE_TIMESTAMP_IN_SECONDS = 1734220799; // 14.12.24 23:59 GMT
+
+export const DELAY_BETWEEN_EACH_REQUEST_IN_SECONDS = 2;
+
+export const TRANSACTION_HASH_OF_KAIDRO_REVERTING_SOME_AKDR =
+  "0x088ec163713befe808c985e644e203c1d69c618a11dd091d6df2b04524d9e947";
