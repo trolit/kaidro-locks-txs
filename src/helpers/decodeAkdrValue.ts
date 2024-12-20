c9aca45b3043b236db7b37c578d3108f30f4a356
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-12-20 12:39:12 +0100

    simplify `decodeAkdrValue` helper

--- src/helpers/decodeAkdrValue.ts
+++ src/helpers/decodeAkdrValue.ts
@@ -1,13 +1,12 @@
 export function decodeAkdrValue(value: string, decimals = 18) {
   const valueDecimals = value.length;
 
-  if (valueDecimals === decimals) {
-    return `0.${value.substring(0, 2)}`;
-  }
-
   const separatorIndex = valueDecimals - decimals;
 
-  const rawValue = `${value.substring(0, separatorIndex)}.${value.substring(separatorIndex)}`;
+  const rawValue =
+    valueDecimals === decimals
+      ? `0.${value}`
+      : `${value.substring(0, separatorIndex)}.${value.substring(separatorIndex)}`;
 
   return parseFloat(rawValue).toFixed(2);
 }
