501ccce3e8240860f766e6c71694e0789faba64f
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-12-20 19:39:07 +0100

    Update useDayjsStore.ts

--- src/store/useDayjsStore.ts
+++ src/store/useDayjsStore.ts
@@ -1,10 +1,27 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { defineStore } from "pinia";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const useDayjsStore = defineStore("useDayjsStore", () => {
   return {
     blockTimeToDate(value: number, format = "DD MMM YYYY") {
       return dayjs(value * 1000).format(format);
+    },
+
+    parseToTimezoneAndFormat(
+      value: string,
+      format = "DD MMM YYYY HH:mm",
+      timezone = "GMT",
+    ) {
+      return dayjs(value).tz(timezone).format(format);
+    },
+
+    getTimestampInSeconds(value: string) {
+      return dayjs(value).unix();
     },
   };
 });
