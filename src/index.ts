b1e1cc21b544bec82f6098ade1558ce35176875f
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-12-20 12:44:32 +0100

    set dark mode

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import ElementPlus from "element-plus";
 
 import "./style.css";
 import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
 
 import App from "./App.vue";
 
